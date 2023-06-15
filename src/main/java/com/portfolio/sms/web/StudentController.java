21a988345b9e0e5d83ca41f62e1907c70de3a43e
CraigWOwen22 <47474473+CraigWOwen22@users.noreply.github.com>
2023-06-15 21:07:04 +0100

    initial commit

---
+++ src/main/java/com/portfolio/sms/web/StudentController.java
@@ -0,0 +1,62 @@
+package com.portfolio.sms.web;
+
+
+import com.portfolio.sms.model.StudentRepository;
+import com.portfolio.sms.model.Student;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+//import jakarta.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id) {
+        return studentRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createStudent(@RequestBody Student student) throws URISyntaxException {
+        Student savedStudent = studentRepository.save(student);
+        return ResponseEntity.created(new URI("/students/" + savedStudent.getId())).body(savedStudent);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        Student currentStudent = studentRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentStudent.setName(student.getName());
+        currentStudent.setEmail(student.getEmail());
+        currentStudent = studentRepository.save(student);
+
+        return ResponseEntity.ok(currentStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteStudent(@PathVariable Long id) {
+        studentRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
