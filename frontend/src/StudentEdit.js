d8467844d81bc6549423b638b931dc77d4319966
CraigWOwen22 <47474473+CraigWOwen22@users.noreply.github.com>
2023-06-24 14:15:50 +0100

    Add further functionality to StudentEdit file

--- frontend/src/StudentEdit.js
+++ frontend/src/StudentEdit.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
@@ -18,5 +19,65 @@ class StudentEdit extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    async componentDidMount() {
+        if (this.props.match.params.id !== 'new') {
+            const student = await (await fetch(`/students/${this.props.match.params.id}`)).json();
+            this.setState({item: student});
+        }
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+    }
+
+async handleSubmit(event) {
+    event.preventDefault();
+    const {item} = this.state;
+
+    await fetch('/students' + (item.id ? '/' + item.id : ''), {
+        method: (item.id) ? 'PUT' : 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item),
+    });
+    this.props.history.push('/students');
 }
-export default withRouter(ClientEdit);
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>{item.id ? 'Edit student' : 'Add student'}</h2>;
+
+        return <div>
+            <AppNavbar/>
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="name">Name</Label>
+                        <Input type="text" name="name" id="name" value={item.name || ''}
+                               onChange={this.handleChange} autoComplete="name"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="text" name="email" id="email" value={item.email || ''}
+                               onChange={this.handleChange} autoComplete="email"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/students">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    }
+}
+
+export default withRouter(studentEdit);
